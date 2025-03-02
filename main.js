edd7c1bdd5d0af8fd450b7d2c15fa5fc611c558f
phcs93 <phcs93@gmail.com>
2025-03-01 22:39:02 -0300

    sub groups

--- main.js
+++ main.js
@@ -17,6 +17,16 @@ const fontPicnum = 2966;
 // corresponding to tile selection menu grid size
 const gridColumNumber = 30;
 
+// smart sorting algorithm
+const smartSort = (a,b) => {
+    if (a.indexOf("#") > -1 && b.indexOf("#") === -1) return -1;
+    if (b.indexOf("#") > -1 && a.indexOf("#") === -1) return +1;
+    if (a.indexOf("#") > -1 && b.indexOf("#") > -1) {
+        return a.split("#")[0] - b.split("#")[0];
+    };
+    return a-b;
+};
+
 const tilesCfgContent = `tilegroup "TAGGED GROUPS" {
 
     hotkey "T"
@@ -25,7 +35,7 @@ const tilesCfgContent = `tilegroup "TAGGED GROUPS" {
 
         ${groups.map(g => {
 
-            const name = g.indexOf(".") > -1 ? g.split(".")[1] : g;
+            const name = g.indexOf("#") > -1 ? g.split("#")[1] : g;
             const nameSprites = name.split("").map(c => fontPicnum + c.toUpperCase().charCodeAt(0) - 65);
 
             const groupNamePicnums = [
@@ -39,15 +49,31 @@ const tilesCfgContent = `tilegroup "TAGGED GROUPS" {
             while (groupNamePicnums.length < gridColumNumber) {
                 groupNamePicnums.push(separatorPicnum);
             }
-
-            const files = fs.readdirSync(`tiles/${g}`).filter(f => f.endsWith(".png"));
-            const tilesPicnums = files.map(f => f.split(".")[0]).sort((a,b) => a-b).concat(...new Array(gridColumNumber - ((files.length % gridColumNumber) || gridColumNumber)).fill(blankPicnum));
+            
+            const tilesPicnums = [];
 
             const subdirs = fs.readdirSync(`tiles/${g}`).filter(f => !f.endsWith(".png"));
             for(const subdir of subdirs) {
-                const subfiles = fs.readdirSync(`tiles/${g}/${subdir}`);
-                tilesPicnums.push(...subfiles.map(f => f.split(".")[0]).sort((a,b) => a-b).concat(...new Array(gridColumNumber - ((subfiles.length % gridColumNumber) || gridColumNumber)).fill(blankPicnum)))
+                const subfiles = fs.readdirSync(`tiles/${g}/${subdir}`).filter(f => f.indexOf("@") === -1);
+                tilesPicnums.push(
+                    ...subfiles
+                    .map(f => f.split(".")[0])                    
+                    .sort(smartSort)
+                    .map(f => f.indexOf("#") > -1 ? f.split("#")[1] : f)
+                    .concat(...new Array(gridColumNumber - ((subfiles.length % gridColumNumber) || gridColumNumber))
+                    .fill(blankPicnum))
+                );
             }
+
+            const files = fs.readdirSync(`tiles/${g}`).filter(f => f.endsWith(".png")).filter(f => f.indexOf("@") === -1);
+            tilesPicnums.push(
+                ...files
+                .map(f => f.split(".")[0])
+                .sort(smartSort)
+                .map(f => f.indexOf("#") > -1 ? f.split("#")[1] : f)
+                .concat(...new Array(gridColumNumber - ((files.length % gridColumNumber) || gridColumNumber))
+                .fill(blankPicnum))
+            );
 
             return `
         // ${name}
